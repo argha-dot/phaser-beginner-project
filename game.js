4fb93917870d5b1402599f96bbee46e9716e7a76
Argha Chakrabarty <ac380012@gmail.com>
2020-10-06 00:45:59 +0530

    added bombs

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+var score = 0;
+var scoreText;
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -41,6 +44,10 @@ function create ()
         setXY: { x: 12, y: 0, stepX: 70 }
     });
 
+    bombs = this.physics.add.group();
+
+    scoreText = this.add.text(16, 16, "score: 0", {fontSize: "32px", fill: "#000000"});
+
     stars.children.iterate(function (child) {
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
     });
@@ -79,6 +86,9 @@ function create ()
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(stars, platforms);
     this.physics.add.overlap(player, stars, collectStar, null, this);
+    this.physics.add.collider(bombs, platforms);
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
+
     cursors = this.input.keyboard.createCursorKeys();
 
 }
@@ -105,4 +115,31 @@ function update () {
 
 function collectStar (player, star) {
     star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0)
+    {
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+    }
+}
+
+function hitBomb (player, bomb) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+    gameOver = true;
 }
